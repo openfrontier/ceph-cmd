d7eacc205768f07bfb536b492866c190b871328c
thinkernel <thinkernel@gmail.com>
2017-03-06 17:45:14 +0800

    make ip address configurable

--- create-ceph-mon.sh
+++ create-ceph-mon.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 set -e
 
+HOST_IP=$1
+CEPH_PUBLIC_NETWORK=$2
+
 docker run -d --net=host --name=ceph-mon-${HOSTNAME} \
+-v /etc/ceph:/etc/ceph \
+-v /etc/ganesha:/etc/ganesha \
 -v /var/lib/ceph:/var/lib/ceph \
--e MON_IP=172.20.201.212 \
--e CEPH_PUBLIC_NETWORK=172.20.201.0/24 \
+-e MON_IP=${HOST_IP} \
+-e CEPH_PUBLIC_NETWORK=${CEPH_PUBLIC_NETWORK} \
 -e KV_TYPE=etcd \
 -e KV_IP=127.0.0.1 \
 -e KV_PORT=2379 \
