f066effa8c5dde6cee440d16e50055283003043d
thinkernel <thinkernel@gmail.com>
2017-03-07 15:04:29 +0800

    switch osd command to osd_ceph_disk

--- create-ceph-osd.sh
+++ create-ceph-osd.sh
@@ -7,9 +7,7 @@ docker run -d --net=host --name=ceph-osd-${HOSTNAME} \
 -v /dev/:/dev/ \
 -v /var/lib/ceph:/var/lib/ceph \
 -e OSD_DEVICE=/dev/sdc \
--e OSD_TYPE=disk \
--e OSD_FORCE_ZAP=1 \
 -e KV_TYPE=etcd \
 -e KV_IP=127.0.0.1 \
 -e KV_PORT=2379 \
-ceph/daemon:tag-build-master-jewel-ubuntu-16.04 osd
+ceph/daemon:tag-build-master-jewel-ubuntu-16.04 osd_ceph_disk
